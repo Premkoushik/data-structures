b5882e9b71a0bcccc2af7d497f02df411ebcc3ed
Premkoushik <91929093+Premkoushik@users.noreply.github.com>
2022-09-13 16:17:58 +0530

    data structures
    user search

---
+++ DS6.cpp
@@ -0,0 +1,16 @@
+void main()
+{
+1.accept num[n], item from the user
+2.for (i=0;i<n;i++)
+{
+   if(item==num[i])
+{
+loc=1;
+break;
+}
+else
+loc=0;
+}
+3.if(loc==1) print search successful 
+else print search unsuccessful
+}
